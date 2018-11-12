dfa517aae2d6f52c526f38fc53d4973860ddc697
Renaldas Pocevicius <project.renaldas@gmail.com>
2018-11-12 13:14:59 +0000

    added new files

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express()
+const port = 3000
+
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs ');
+
+app.use((req, res, next) =>{
+  let now = new Date().toString(); 
+  let log = `${now}: ${req.method} ${req.originalUrl}`;
+
+  fs.appendFile( 'server.log', log +'\n', (err) => {
+    if(err){
+      console.log('Unable to add data to the server.log!!!');
+    };
+
+  });
+  console.log();
+  next();
+});;
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance',
+//     content: 'Back on 24hr.'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle:'Home page',
+    content: 'Some content'
+  });
+});
+
+app.get('/about', (req, res) => {
+  
+  res.render('about.hbs', {
+    pageTitle:'About page',
+    content: 'Some content'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: ' Unable to connect to server'
+  })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
